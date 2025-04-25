9898c919f493741c5252ef18c5ffa87674373d6f
GuiHeinrichs <heinrichsgui@gmail.com>
2025-04-25 19:57:46 -0300

    dsadasdas

--- src/services/get-sports-odds.js
+++ src/services/get-sports-odds.js
@@ -1,7 +1,9 @@
+const apiKey = process.env.NEXT_PUBLIC_ODD_API_KEY;
+
 export default async function getOddsBy(dataObject) {
   try {
     const response = await fetch(
-      `https://api.the-odds-api.com/v4/sports/${dataObject.sport}/odds/?regions=${dataObject.regions}&markets=${dataObject.markets}&apiKey=${process.env.NEXT_PUBLIC_ODD_API_KEY}`,
+      `https://api.the-odds-api.com/v4/sports/${dataObject.sport}/odds/?regions=${dataObject.regions}&markets=${dataObject.markets}&apiKey=${apiKey}`,
     );
     const odds = await response.json();
     return odds;
